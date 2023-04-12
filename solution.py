b8b6f49e65e5202b8af4b7fe7ce92f5c3f04899c
Daria Tkachenko <109956269+RuGernika@users.noreply.github.com>
2023-04-12 23:22:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ import numpy as np
 chat_id = 1902092480  # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: 
-    alpha = 0.02
+    alpha = 0.04
     stat, pval  = ztest(x, value=500, alternative='larger')
     return pval < alpha. # Ваш ответ, True или False
