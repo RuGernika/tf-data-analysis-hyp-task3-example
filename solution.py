455119b5c5c86841b587ef5e1797149ff7f8c74f
Daria Tkachenko <109956269+RuGernika@users.noreply.github.com>
2023-04-12 23:22:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,7 @@ import numpy as np
 
 chat_id = 1902092480  # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool: 
+    alpha = 0.02
+    stat, pval  = ztest(x, value=500, alternative='larger')
+    return pval < alpha. # Ваш ответ, True или False
