fc9dd6126b1f932567d202df9f8d196ae1250c34
Daria Tkachenko <109956269+RuGernika@users.noreply.github.com>
2023-04-13 11:03:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,22 @@ from scipy import stats
 
 chat_id = 1902092480  # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: 
-    alpha = 0.04
-    stat, pval  = ztest(x, value=500, alternative='larger')
-    return pval < alpha # Ваш ответ, True или False
+def solution(data1: int, data2: int)-> bool:
+  from scipy.stats import t
+
+  mean1 = np.mean(data1)
+  mean2 = np.mean(data2)
+  std1 = np.std(data1, ddof=1)
+  std2 = np.std(data2, ddof=1)
+  n1 = len(data1)
+  n2 = len(data2)
+  t_stat = (mean1 - mean2) / np.sqrt((std1**2 / n1) + (std2**2 / n2))
+  alpha = 0.04
+  df = n1 + n2 - 2
+  critical_value = t.ppf(1 - alpha, df)
+
+  if t_stat > critical_value or t_stat < -critical_value:
+    rez = 1
+  else:
+    rez = 0 
+  return bool(rez)
